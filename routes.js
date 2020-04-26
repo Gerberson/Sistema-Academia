26173bcce9be587483b2a007c7cb5db7ab469520
Gerberson Dias <gerbersondias.312@gmail.com>
2020-04-26 11:59:51 -0300

    Validando os campos

--- routes.js
+++ routes.js
@@ -14,6 +14,15 @@ routes.get('/instructors/create', function(req, res){
 })
 
 routes.post('/instructors', function(req, res){
+
+    // Transforma o json em objeto(array)
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if (req.body[key] == "")
+            return res.send('Preencha todos os campos!')
+    }
+
     return res.send(req.body)
 })
 
