958f798616c96dd681cf53bdefb82530da3833ba
Gerberson Dias <gerbersondias.312@gmail.com>
2020-04-26 19:06:17 -0300

    Formatando data nascimento para idade e data format

--- instructors.js
+++ instructors.js
@@ -1,6 +1,7 @@
 // Serve para criar json com os dados simulando o BD
 const fs = require('fs')
 const data = require('./data.json')
+const { age } = require('./utils')
 
 //req.query.id = ?id=1
 //req.body
@@ -17,9 +18,9 @@ exports.findById = function(req, res) {
 
     const instructor = {
         ...foundInstructor,
-        age: "",
+        age: age(foundInstructor.birth),
         services: foundInstructor.services.split(","),
-        created_at: "",
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
     }
 
     return res.render('instructors/show', { instructor })
