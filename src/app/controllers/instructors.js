8a91553c59ba76a17a904f9497e04cc477a99d98
Gerberson Dias <gerbersondias.312@gmail.com>
2020-05-01 05:33:47 -0300

    Inserindo dados no banco

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,4 +1,5 @@
 const { age, date } = require('../../lib/utils')
+const db = require('../../config/db')
 
 module.exports = {
     index(req, res) {
@@ -18,11 +19,34 @@ module.exports = {
             if (req.body[key] == "")
                 return res.send('Preencha todos os campos!')
         }
-        req.body.birth = Date.parse(req.body.birth)
-        req.body.created_at = Date.now()
-        req.body.id = Number(bd.instructors.length + 1)
+        
+        const query =`
+            INSERT INTO instructors (
+                name,
+                avatar_url,
+                gender,
+                services,
+                birth,
+                created_at
+            ) VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING id
+        `
 
-        return
+        const values = [
+            req.body.name,
+            req.body.avatar_url,
+            req.body.gender,
+            req.body.services,
+            date(req.body.birth).iso,
+            date(Date.now()).iso
+        ]
+
+        db.query(query, values, function(err, result){
+            if (err) return res.send('Database Error!')
+
+            return res.redirect(`/instructors/${result.rows[0].id}`)
+        }) 
+            
     },
     edit(req, res) {
         return
